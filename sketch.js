55f5ee4f1d5e660ac413d17b74853aa7667189d0
DSGAMING98 <74608619+DSGAMING98@users.noreply.github.com>
2020-11-27 18:54:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,10 +17,10 @@ function draw(){
     bullet.velocityX=0;
     var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
     if(damage>10){
-      wall.shapeColor=color(255,0,0);
+      wall.shapeColor="red";
     }
     if(damage<10){
-      wall.shapeColor=color(0,255,0);
+      wall.shapeColor="red";
     }
   }
     drawSprites();
